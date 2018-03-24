65a8a78722287ca3fdcff2440f8defe409700631
JonoCX <jonathan.carlton@hotmail.com>
2018-03-24 19:26:17 +0000

    Initial commit including the paper object model

---
+++ src/main/java/uk/ac/manchester/cs/iam/litreviewtool/Main.java
@@ -0,0 +1,12 @@
+package uk.ac.manchester.cs.iam.litreviewtool;
+
+import org.apache.commons.cli.Options;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/uk/ac/manchester/cs/iam/litreviewtool/models/Paper.java
@@ -0,0 +1,94 @@
+package uk.ac.manchester.cs.iam.litreviewtool.models;
+
+import java.io.Serializable;
+
+/**
+ * @author Jonathan Carlton
+ */
+public class Paper implements Serializable {
+
+    private String title;
+    private String author;
+    private String year;
+    private String abs;
+    private String keywords;
+
+    public Paper() { }
+
+    public Paper(String title, String author, String year, String abs, String keywords) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.abs = abs;
+        this.keywords = keywords;
+    }
+
+    @Override
+    public String toString() {
+        return title + ", " + author + ", " + year + ", " + abs + ", " + keywords;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Paper paper = (Paper) o;
+
+        if (!title.equals(paper.title)) return false;
+        if (!author.equals(paper.author)) return false;
+        if (!year.equals(paper.year)) return false;
+        if (!abs.equals(paper.abs)) return false;
+        return keywords.equals(paper.keywords);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        result = 31 * result + year.hashCode();
+        result = 31 * result + abs.hashCode();
+        result = 31 * result + keywords.hashCode();
+        return result;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getAbs() {
+        return abs;
+    }
+
+    public void setAbs(String abs) {
+        this.abs = abs;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+}
